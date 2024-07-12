59c7a601bd74d89c8434d43ed4f137dfdb776658
cle-tort <c.letortorec56@gmail.com>
2024-07-12 03:59:49 +0200

    save

--- src/app/forms/AddRental.tsx
+++ src/app/forms/AddRental.tsx
@@ -50,100 +50,91 @@ export default function AddRental({ onClose }) {
   return (
     <View>
 
-    <KeyboardAvoidingView
-    behavior='padding'
-    >
-
-    <SafeAreaView>
-      <View id="Logo_and_text" className='flex-row justify-between items-start'>
-        <TouchableOpacity className="ml-4 absolute"onPress={onClose}>
-          <Ionicons name="close-circle" size={40} color="#736ced" />
-        </TouchableOpacity>
-        <View className="flex-row flex-1 justify-center items-center ml-4 mt-14 rounded-lg p-2 border-primary border-2"> 
-          <Text className='flex-wrap text-center flex-1 font-pmedium'>
-            Veuillez entrer le nom et les caractéristiques du bien à ajouter.
-          </Text>
+      <KeyboardAvoidingView behavior='padding'>
+        <SafeAreaView>
+            <TouchableOpacity className="ml-4 "onPress={onClose}>
+              <Ionicons name="close-circle" size={40} color="#736ced" />
+            </TouchableOpacity>
+          <View id="Logo_and_text" className='flex-row justify-between items-start'>
+            <View className="flex-row flex-1 justify-center items-center ml-4 mt-14 rounded-lg p-2 border-primary border-2"> 
+              <Text className='flex-wrap text-center flex-1 font-pmedium'>
+                Veuillez entrer le nom et les caractéristiques du bien à ajouter.
+              </Text>
+            </View>
+            <Image 
+              source={images.logo}
+              className="w-[115px] h-[100px] mt-7"
+              resizeMode='contain'
+            />
           </View>
-          <Image 
-            source={images.logo}
-            className="w-[115px] h-[100px] mt-7"
-            resizeMode='contain'
-          />
-      </View>
-        <ScrollView className='mx-6 flex my-auto flex-grow mt-10'>
-          <Text className='font-pregular text-xl mb-2'>Nom</Text>
-          <TextInput
-            style={styles.input}
-            value={newRental.rental_name}
-            onChangeText={(text) => setNewRental({ ...newRental, rental_name: text })}
-            placeholder="Studio Rivoli"
+          <ScrollView className='mx-6 flex my-auto flex-grow mt-5'>
+            <Text className='font-pregular text-xl mb-2'>Nom</Text>
+            <TextInput
+              style={styles.input}
+              value={newRental.rental_name}
+              onChangeText={(text) => setNewRental({ ...newRental, rental_name: text })}
+              placeholder="Studio Rivoli"
             />
-          <View className='flex-row gap-4'>
-          <View className='flex-1'>
-           <Text className='font-pregular text-xl mb-2'>Ville</Text>
-          <TextInput
-            style={styles.input}
-            value={newRental.rental_city}
-            onChangeText={(text) => setNewRental({ ...newRental, rental_city: text })}
-            placeholder="Paris"
-            />
-
-            </View>
-            <View className='flex-1'>
-          <Text className='font-pregular text-xl mb-2'>Code postal</Text>
-          <TextInput
-            style={styles.input}
-            value={newRental.rental_postal_code}
-            onChangeText={(text) => setNewRental({ ...newRental, rental_postal_code: text })}
-            placeholder="75004"
-            keyboardType="numeric"
-            />
-            </View>
-            </View>
-
-
-          <Text className='font-pregular text-xl mb-2'>Rue</Text>
-          <TextInput
-            style={styles.input}
-            value={newRental.rental_street}
-            onChangeText={(text) => setNewRental({ ...newRental, rental_street: text })}
-            placeholder="45 rue François Miron"
-          />
-           <View className='flex-row gap-4'>
+            <View className='flex-row gap-4'>
               <View className='flex-1'>
-                  <Text className='font-pregular text-xl mb-2'>Pays</Text>
-                  <TextInput
-                    style={styles.input}
-                    value={newRental.country}
-                    onChangeText={(text) => setNewRental({ ...newRental, country: text })}
-                    placeholder="France"
-                  />
+                <Text className='font-pregular text-xl mb-2'>Ville</Text>
+                <TextInput
+                  style={styles.input}
+                  value={newRental.rental_city}
+                  onChangeText={(text) => setNewRental({ ...newRental, rental_city: text })}
+                  placeholder="Paris"
+                />
               </View>
-              
+              <View className='flex-1'>
+                <Text className='font-pregular text-xl mb-2'>Code postal</Text>
+                <TextInput
+                  style={styles.input}
+                  value={newRental.rental_postal_code}
+                  onChangeText={(text) => setNewRental({ ...newRental, rental_postal_code: text })}
+                  placeholder="75004"
+                  keyboardType="numeric"
+                />
+              </View>
+            </View>
+            <Text className='font-pregular text-xl mb-2'>Rue</Text>
+            <TextInput
+              style={styles.input}
+              value={newRental.rental_street}
+              onChangeText={(text) => setNewRental({ ...newRental, rental_street: text })}
+              placeholder="45 rue François Miron"
+            />
+            <View className='flex-row gap-4'>
+              <View className='flex-1'>
+                <Text className='font-pregular text-xl mb-2'>Pays</Text>
+                <TextInput
+                  style={styles.input}
+                  value={newRental.country}
+                  onChangeText={(text) => setNewRental({ ...newRental, country: text })}
+                  placeholder="France"
+                />
+              </View>
               <View className=' flex-1'>
-                  <Text className='font-pregular text-xl mb-2'>Surface (m²)</Text>
-                  <TextInput
-                    style={styles.input}
-                    value={newRental.surface_area}
-                    onChangeText={(text) => setNewRental({ ...newRental, surface_area: text })}
-                    placeholder="31"
-                    keyboardType="numeric"
-                  />
+                <Text className='font-pregular text-xl mb-2'>Surface (m²)</Text>
+                <TextInput
+                  style={styles.input}
+                  value={newRental.surface_area}
+                  onChangeText={(text) => setNewRental({ ...newRental, surface_area: text })}
+                  placeholder="31"
+                  keyboardType="numeric"
+                />
               </View>
             </View>          
-
-        </ScrollView>
-    </SafeAreaView>
-  </KeyboardAvoidingView>
-          <View className='justify-center w-full items-center mt-10'>
-          <CustomButton 
-              handlePress={addNewRental}
-              title={"ENREGISTRER"}
-              containerStyles={"w-3/4"}
-            />
-          </View>
-          </View>
-
+          </ScrollView>
+        </SafeAreaView>
+      </KeyboardAvoidingView>
+      <View className='justify-center w-full items-center mt-10'>
+        <CustomButton 
+          handlePress={addNewRental}
+          title={"ENREGISTRER"}
+          containerStyles={"w-3/4"}
+        />
+      </View>
+    </View>
   );
 }
 
