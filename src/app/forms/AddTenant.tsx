b2fc15f10fafaade1448bbbeadff7e022d02edcd
cle-tort <c.letortorec56@gmail.com>
2024-07-12 10:25:37 +0200

    save

--- src/app/forms/AddTenant.tsx
+++ src/app/forms/AddTenant.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, 
-  StyleSheet, Platform, KeyboardAvoidingView, Image, Button } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Platform, KeyboardAvoidingView, Image, Alert } from 'react-native';
 import { useSQLiteContext } from 'expo-sqlite/next';
 import { Ionicons } from '@expo/vector-icons';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { ScrollView } from 'react-native-gesture-handler';
-import { images } from '../../constants'
+import { images } from '../../constants';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import CustomButton from '@/components/CustomButton';
 
 export default function AddTenant({ rentalId, onClose }) {
   const [newTenant, setNewTenant] = useState({
@@ -14,26 +14,35 @@ export default function AddTenant({ rentalId, onClose }) {
     last_name: '',
     rent_amount: '',
     move_in_date: new Date(),
-    rent_payment_date: new Date(),
+    rent_payment_day: '', // Changed from rent_payment_date to rent_payment_day
   });
-  const [showMoveInDatePicker, setShowMoveInDatePicker] = useState(false);
-  const [showRentPaymentDatePicker, setShowRentPaymentDatePicker] = useState(false);
 
   const db = useSQLiteContext();
 
   const handleMoveInDateChange = (event, selectedDate) => {
     const currentDate = selectedDate || newTenant.move_in_date;
-    setShowMoveInDatePicker(Platform.OS === 'ios');
     setNewTenant({ ...newTenant, move_in_date: currentDate });
   };
 
-  const handleRentPaymentDateChange = (event, selectedDate) => {
-    const currentDate = selectedDate || newTenant.rent_payment_date;
-    setShowRentPaymentDatePicker(Platform.OS === 'ios');
-    setNewTenant({ ...newTenant, rent_payment_date: currentDate });
+  const validateInputs = () => {
+    if (
+      !newTenant.first_name ||
+      !newTenant.last_name ||
+      !newTenant.rent_amount ||
+      !newTenant.rent_payment_day ||
+      isNaN(newTenant.rent_payment_day) ||
+      newTenant.rent_payment_day < 1 ||
+      newTenant.rent_payment_day > 28
+    ) {
+      Alert.alert('Incomplet', 'Veuillez remplir tous les champs et rentrer un jour de paiement entre 1 et 28.');
+      return false;
+    }
+    return true;
   };
 
   async function addNewTenant() {
+    if (!validateInputs()) return;
+
     await db.withTransactionAsync(async () => {
       await db.runAsync(
         `INSERT INTO Tenant (first_name, last_name, rent_amount, move_in_date, rent_payment_date, rental_id) VALUES (?, ?, ?, ?, ?, ?)`,
@@ -42,7 +51,7 @@ export default function AddTenant({ rentalId, onClose }) {
           newTenant.last_name,
           parseFloat(newTenant.rent_amount),
           newTenant.move_in_date.toISOString(),
-          newTenant.rent_payment_date.toISOString(),
+          newTenant.rent_payment_day, // Store only the day of the month
           rentalId,
         ]
       );
@@ -58,12 +67,12 @@ export default function AddTenant({ rentalId, onClose }) {
             <Ionicons name="close-circle" size={40} color="#736ced" />
           </TouchableOpacity>
           <View id="Logo_and_text" className='flex-row justify-between items-start'>
-            <View className="flex-row flex-1 justify-center items-center ml-4 mt-14 rounded-lg p-2 border-primary border-2"> 
+            <View className="flex-row flex-1 justify-center items-center ml-4 mt-10 rounded-lg p-2 border-primary border-2">
               <Text className='flex-wrap text-center flex-1 font-pmedium'>
                 Veuillez entrer le nom et les caractéristiques du locataire à ajouter.
               </Text>
             </View>
-            <Image 
+            <Image
               source={images.logo}
               className="w-[115px] h-[100px] mt-7"
               resizeMode='contain'
@@ -75,50 +84,49 @@ export default function AddTenant({ rentalId, onClose }) {
               style={styles.input}
               value={newTenant.first_name}
               onChangeText={(text) => setNewTenant({ ...newTenant, first_name: text })}
-              placeholder="John"
+              placeholder="Tony"
             />
             <Text className='font-pregular text-xl mb-2'>Nom</Text>
             <TextInput
               style={styles.input}
               value={newTenant.last_name}
               onChangeText={(text) => setNewTenant({ ...newTenant, last_name: text })}
-              placeholder="Doe"
+              placeholder="Soprano"
             />
             <Text className='font-pregular text-xl mb-2'>Montant du loyer (€)</Text>
             <TextInput
               style={styles.input}
               value={newTenant.rent_amount}
               onChangeText={(text) => setNewTenant({ ...newTenant, rent_amount: text })}
-              placeholder="1000"
+              placeholder="450"
               keyboardType="numeric"
             />
-            <TouchableOpacity onPress={() => setShowMoveInDatePicker(true)}>
-              <Text style={styles.datePickerText}>Move-in Date: {newTenant.move_in_date.toDateString()}</Text>
-            </TouchableOpacity>
-            {showMoveInDatePicker && (
+            <View className='flex-row mb-6 items-center'>
+              <Text className='font-pregular text-xl'>Date d'entrée</Text>
               <DateTimePicker
                 value={newTenant.move_in_date}
                 mode="date"
                 display="default"
                 onChange={handleMoveInDateChange}
               />
-            )}
-            <TouchableOpacity onPress={() => setShowRentPaymentDatePicker(true)}>
-              <Text style={styles.datePickerText}>Rent Payment Date: {newTenant.rent_payment_date.toDateString()}</Text>
-            </TouchableOpacity>
-            {showRentPaymentDatePicker && (
-              <DateTimePicker
-                value={newTenant.rent_payment_date}
-                mode="date"
-                display="default"
-                onChange={handleRentPaymentDateChange}
-              />
-            )}
+            </View>
+            <Text className='font-pregular text-xl mb-2'>Jour estimé de paiement du loyer</Text>
+            <TextInput
+              style={styles.input}
+              value={newTenant.rent_payment_day}
+              onChangeText={(text) => setNewTenant({ ...newTenant, rent_payment_day: text })}
+              placeholder="15"
+              keyboardType="numeric"
+            />
           </ScrollView>
         </SafeAreaView>
       </KeyboardAvoidingView>
-      <View className='justify-center w-full items-center mt-10'>
-        <Button title="Add Tenant" onPress={addNewTenant} />
+      <View className='justify-center w-full items-center'>
+        <CustomButton
+          handlePress={addNewTenant}
+          title={"ENREGISTRER"}
+          containerStyles={"w-3/4"}
+        />
       </View>
     </View>
   );
@@ -137,9 +145,5 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginBottom: 20,
     paddingHorizontal: 10,
-  },
-  datePickerText: {
-    color: 'blue',
-    marginBottom: 10,
   },
 });
